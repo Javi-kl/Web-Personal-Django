0002048d3e910c13579d848bf02c7f90af717499
Javi-kl <203809377+Javi-kl@users.noreply.github.com>
2026-04-03 19:56:41 +0200

    feat: añadir soporte de imágenes múltiples a proyectos - Modelo ProjectImage (ForeignKey, related_name='images') - FormSet para subir 3 imágenes porproyecto - Admin inline para gestión centralizada - STORAGES configurado para media + static - Volumen Docker persistente para uploads - Templates: imagen principal en home, galería en detalle

--- projects/forms.py
+++ projects/forms.py
@@ -1,6 +1,26 @@
 from django import forms
 
-from .models import Comment
+from .models import Comment, ProjectImage, ProjectModel
+
+
+class ProjectModelForm(forms.ModelForm):
+    class Meta:
+        model = ProjectModel
+        fields = ["title", "description", "github_url", "order"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "form-control"})
+
+
+ProjectImageFormSet = forms.inlineformset_factory(
+    ProjectModel,
+    ProjectImage,
+    fields=["image", "order"],
+    extra=3,
+    can_delete=True,
+)
 
 
 class CommentForm(forms.ModelForm):
